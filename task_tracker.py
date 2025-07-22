a4d3b48e10c1f8cdc337aebcb4c79cd839128d8c
chapuwu <lolochapa10@hotmail.com>
2025-07-22 13:38:25 -0300

    se termino la funcion de add_task

--- task_tracker.py
+++ task_tracker.py
@@ -1,30 +1,33 @@
 import argparse
 import json
-from dataclasses import dataclass
+from datetime import date
 
-@dataclass
-class Task:
-    id: int
-    description: str
-    status: str
-    createdAt: str
-    updatedAt: str
+try:
+    with open('datos.json', 'r') as file:
+        task_arr = json.load(file)
+except:
+    print('El archivo esta vacio... inicializando')
+    with open('datos.json', 'w') as file:
+        file.write('{''"tasks": []}')
 
 def add_task(description):
-    try:
-        with open('datos.json', 'r') as file:
-            data = json.load(file)
-    except:
-        print('El archivo esta vacio, inicializando...')
-        with open('datos.json', 'w') as file:
-            file.write("{}")
+    today = date.today()
 
-    task: Task = {
-        "id": int,
+    if (len(task_arr["tasks"]) == 0):
+        new_id = 0
+    else:
+        new_id = task_arr["tasks"][-1]["id"] + 1
+
+    task = {
+        "id": new_id,
         "description": description,
-        "status": str
+        "createdAt": str(today)
     }
 
+    task_arr["tasks"].append(task)
+
+    with open('datos.json', 'w') as file:
+        json.dump(task_arr, file, indent=4)
 
 parser = argparse.ArgumentParser(prog='task-cli')
 subparsers = parser.add_subparsers(dest='command')
