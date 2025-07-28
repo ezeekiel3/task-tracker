83247e3a634787be3e684a54d54528fad83a8606
ezeekiel3 <lolochapa10@hotmail.com>
2025-07-28 18:42:04 -0300

    project finished

--- task_tracker.py
+++ task_tracker.py
@@ -21,6 +21,7 @@ def add_task(description):
     task = {
         "id": new_id,
         "description": description,
+        "status": "todo",
         "createdAt": str(today)
     }
 
@@ -49,13 +50,101 @@ def delete_task(task_id):
             return
     print("no se encontro la tarea con el id: ", task_id)
 
-# def task_in_progress(task_id):
+def task_in_progress(task_id):
+    for i, task in enumerate(task_arr["tasks"]):
+        if (task["id"] == task_id):
+            select_item_by_id = task_arr["tasks"][i]
+    select_item_by_id["status"] = "in-progress"
+    
+    with open('datos.json', "w") as file:
+        json.dump(task_arr, file, indent=4)
 
+def task_done(task_id):
+    for i, task in enumerate(task_arr["tasks"]):
+        if (task["id"] == task_id):
+            select_item_by_id = task_arr["tasks"][i]
+    
+    select_item_by_id["status"] = "done"
+    with open('datos.json', "w") as file:
+        json.dump(task_arr, file, indent=4)
 
-# def task_done(task_id):    
+def show_list(status=""):
+    if (status == "done"):
+        task_done_founded = False
+        for task in task_arr["tasks"]:
+            if (task["status"] == "done"):
+                task_done_founded = True
+                print(task)
+        if (task_done_founded):
+            return
+        else:
+            print('no se encontro ninguna tarea terminada')
+            return
+    
+    if (status == "in-progress"):
+        task_progress_founded = False
+        for task in task_arr["tasks"]:
+            if (task["status"] == "in-progress"):
+                task_progress_founded = True
+                print(task)
+        if (task_progress_founded):
+            return
+        else:
+            print('no se encontro ninguna tarea en progreso')
+            return
+    
+    if (status == "todo"):
+        task_todo_founded = False
+        for task in task_arr["tasks"]:
+            if (task["status"] == "todo"):
+                task_todo_founded = True
+                print(task)
+        if (task_todo_founded):
+            return
+        else:
+            print('no se encontro ninguna tarea para hacer')
+            return
+
+    for task in task_arr["tasks"]:
+        print(task)
+    
 
 parser = argparse.ArgumentParser(prog='task-cli')
 subparsers = parser.add_subparsers(dest='command')
 
 add_parser = subparsers.add_parser('add')
 add_parser.add_argument('description', type=str)
+
+update_parser = subparsers.add_parser('update')
+update_parser.add_argument('id', type=int)
+update_parser.add_argument('description', type=str)
+
+delete_parser = subparsers.add_parser('delete')
+delete_parser.add_argument('id', type=int)
+
+progress_parser = subparsers.add_parser('mark-in-progress')
+progress_parser.add_argument('id', type=int)
+
+done_parser = subparsers.add_parser('mark-done')
+done_parser.add_argument('id', type=int)
+
+show_list_parser = subparsers.add_parser('list')
+show_list_parser.add_argument('status', nargs='?', choices=['done', 'todo', 'in-progress'])
+
+args = parser.parse_args()
+
+match args.command:
+    case 'add':
+        add_task(args.description)
+    case 'update':
+        update_task(args.id, args.description)
+    case 'delete':
+        delete_task(args.id)
+    case 'mark-in-progress':
+        task_in_progress(args.id)
+    case 'mark-done':
+        task_done(args.id)
+    case 'list':
+        show_list(args.status)
+    case _:
+        parser.print_help()
